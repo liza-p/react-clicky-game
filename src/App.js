c1ba4d7bd837f9557ede8c5c1b7c56903838d2d7
liza p <liyourlp@gmail.com>
2020-08-10 20:32:27 -0700

    add App function to render componnents

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './components/Navbar';
+import Wrapper from './components/Wrapper';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Wrapper/>
+    </div>
+  );
+}
+
+export default App;
