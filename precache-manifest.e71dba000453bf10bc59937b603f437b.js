50538db3f512880feeb8a30769d888c3045e05fe
liza p <liyourlp@gmail.com>
2020-08-11 20:36:45 -0700

    Updates

---
+++ precache-manifest.e71dba000453bf10bc59937b603f437b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee190d19b9082d7e954ea80b4f325e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d059720c27a6abb799c",
+    "url": "/static/css/main.4a82cd6f.chunk.css"
+  },
+  {
+    "revision": "15dcc2c7fb5639eed3be",
+    "url": "/static/js/2.1f6deb50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f6deb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d059720c27a6abb799c",
+    "url": "/static/js/main.7ceddd71.chunk.js"
+  },
+  {
+    "revision": "22eabea025eba006b39b",
+    "url": "/static/js/runtime-main.34b1f622.js"
+  }
+]);
