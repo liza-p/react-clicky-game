250224087a069fe58e00fa6b19a46706ec7328ea
liza p <liyourlp@gmail.com>
2020-08-11 20:42:48 -0700

    Show the message & score in navbar from the state

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="navbar container-fluid navbar-expand-lg navbar-light bg-light">
       <div className="row" style={{width:"100%"}}>
@@ -10,10 +10,10 @@ function Navbar() {
         <a className="navbar-brand" href="#">Clicky Game</a>
         </div>
         <div className ="col-4 navbar-brand mr-0">
-         Text
+         {props.message}
         </div>
         <div className ="col-4 navbar-brand mr-0">
-          Score
+          Score {props.score}
         </div>
       </div>
     </nav>
